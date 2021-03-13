ac7a60d79d95d61a5ff3fd98e402bf8dd9b385c5
steph <sbullier@gmail.com>
2021-03-13 13:56:03 +0100

    Change let to const

--- js/displayPosts.js
+++ js/displayPosts.js
@@ -3,18 +3,18 @@ import {getPosts} from './getPosts.js'
 export const displayPosts = async () => {
   let posts = []
   const appHtml = document.querySelector('#app')
-
+  
   try {
     posts = await getPosts()
-
-    for (let post of posts) {
-        appHtml.insertAdjacentHTML('beforeend', `
-                                                  <div>
-                                                    <h2>${post.title}</h2>
-                                                    <p>${post.body}</p>
-                                                  </div>
-                                                `
-                                  )
+    
+    for (const post of posts) {
+      appHtml.insertAdjacentHTML('beforeend', `
+                                                <div>
+                                                  <h2>${post.title}</h2>
+                                                  <p>${post.body}</p>
+                                                </div>
+                                              `
+                                )
     }
   } catch (error) {
     console.error(error.message)
